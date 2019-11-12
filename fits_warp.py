6f1282e4b875a1beb47e72384faa95b0aa9feec3
Paul Hancock <Paul.Hancock@curtin.edu.au>
2019-11-12 11:32:16 +0800

    use pprocess from AegeanTools

--- fits_warp.py
+++ fits_warp.py
@@ -20,7 +20,7 @@ import psutil
 # Parallelise the code
 import multiprocessing
 # multiple cores support
-import pprocess
+from AegeanTools import pprocess
 
 __author__ = ["Natasha Hurley-Walker","Paul Hancock"]
 __date__ = "2019-08-08"
