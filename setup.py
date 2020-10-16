53a7958cef490127234c7cc48497163fe4882f8c
Tim Galvin <tgalvin@curtin.edu.au>
2020-10-16 14:43:58 +0800

    added setup.py
    will install fits_warp.py as a module and ;et it be a callable script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+reqs = [
+    "astropy",
+    "numpy",
+    "scipy",
+    "matplotlib",
+    "psutil",
+]
+
+setup(
+    name='fits_warp',
+    version='2.0',
+    author="Natasha Hurley-Walker and Paul Hancock",
+    description="Warp catalogues and images to remove the distorting affect of the ionosphere.",
+    url="https://github.com/nhurleywalker/fits_warp",
+    long_description=open('README.md').read(),
+    packages=['fits_warp',],
+    license='Academic Free License v3.0',
+    requires=reqs,
+    scripts=["fits_warp/fits_warp.py"],
+)
